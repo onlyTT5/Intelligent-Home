4ac2905592fc4d2fc1004bb07206635de3001afc
onlyTT5 <2218201514@qq.com>
2025-08-12 17:09:40 +0800

    ready arm

--- project/UI/MQTT.c
+++ project/UI/MQTT.c
@@ -4,11 +4,23 @@
 // 全局变量，用于在MQTT回调和UI线程间传递数据
 typedef struct
 {
-    char music_action[10];   // "next", "prev", "on", "off"
-    char light_action[10];   // "on", "off"
-    char curtain_action[10]; // "on", "off"
-    char air_action[10];     // "on", "off"
-    int need_update;         // 标记是否需要更新UI
+    char music1_action[10]; // "next", "prev", "on", "off"
+    char music2_action[10]; // "next", "prev", "on", "off"
+    char music3_action[10]; // "next", "prev", "on", "off"
+
+    char light1_action[10]; // "on", "off"
+    char light2_action[10]; // "on", "off"
+    char light3_action[10]; // "on", "off"
+
+    char curtain1_action[10]; // "on", "off"
+    char curtain2_action[10]; // "on", "off"
+    char curtain3_action[10]; // "on", "off"
+
+    char air1_action[10]; // "on", "off"
+    char air2_action[10]; // "on", "off"
+    char air3_action[10]; // "on", "off"
+
+    int need_update; // 标记是否需要更新UI
 } mqtt_ui_data_t;
 
 static mqtt_ui_data_t g_mqtt_data = {0};
@@ -21,76 +33,223 @@ static void ui_update_timer_cb(lv_timer_t *timer)
         return;
     }
 
+    // 客厅的组建
     // 处理音乐控制
-    if (strlen(g_mqtt_data.music_action) > 0)
+    if (strlen(g_mqtt_data.music1_action) > 0)
     {
-        if (strcmp(g_mqtt_data.music_action, "next") == 0)
+        if (strcmp(g_mqtt_data.music1_action, "next") == 0)
         {
             nextSong(NULL, ui_next, ui_musicInfo, ui_songName, ui_singer);
         }
-        else if (strcmp(g_mqtt_data.music_action, "prev") == 0)
+        else if (strcmp(g_mqtt_data.music1_action, "prev") == 0)
         {
             prevSong(NULL, ui_last, ui_musicInfo, ui_songName, ui_singer);
         }
-        else if (strcmp(g_mqtt_data.music_action, "on") == 0)
+        else if (strcmp(g_mqtt_data.music1_action, "on") == 0)
         {
             playSong(NULL, ui_play, ui_musicInfo, ui_songName, ui_singer);
         }
-        else if (strcmp(g_mqtt_data.music_action, "off") == 0)
+        else if (strcmp(g_mqtt_data.music1_action, "off") == 0)
         {
             playSong(NULL, ui_play, ui_musicInfo, ui_songName, ui_singer);
         }
-        memset(g_mqtt_data.music_action, 0, sizeof(g_mqtt_data.music_action));
+        memset(g_mqtt_data.music1_action, 0, sizeof(g_mqtt_data.music1_action));
     }
 
     // 处理灯光控制
-    if (strlen(g_mqtt_data.light_action) > 0)
+    if (strlen(g_mqtt_data.light1_action) > 0)
     {
-        if (strcmp(g_mqtt_data.light_action, "on") == 0)
+        if (strcmp(g_mqtt_data.light1_action, "on") == 0)
         {
             lightAllOn(NULL, ui_lightOnImg, ui_lightOffImg, ui_lightSliderValue, ui_lightSlider);
         }
-        else if (strcmp(g_mqtt_data.light_action, "off") == 0)
+        else if (strcmp(g_mqtt_data.light1_action, "off") == 0)
         {
             lightAllOff(NULL, ui_lightOnImg, ui_lightOffImg, ui_lightSliderValue, ui_lightSlider);
         }
-        memset(g_mqtt_data.light_action, 0, sizeof(g_mqtt_data.light_action));
+        memset(g_mqtt_data.light1_action, 0, sizeof(g_mqtt_data.light1_action));
     }
 
     // 处理窗帘控制
-    if (strlen(g_mqtt_data.curtain_action) > 0)
+    if (strlen(g_mqtt_data.curtain1_action) > 0)
     {
-        if (strcmp(g_mqtt_data.curtain_action, "on") == 0)
+        if (strcmp(g_mqtt_data.curtain1_action, "on") == 0 || strcmp(g_mqtt_data.curtain1_action, "open") == 0)
         {
             curtainAllOn(NULL, ui_curtainOnImg, ui_curtainOffImg);
         }
-        else if (strcmp(g_mqtt_data.curtain_action, "off") == 0)
+        else if (strcmp(g_mqtt_data.curtain1_action, "off") == 0 || strcmp(g_mqtt_data.curtain1_action, "close") == 0)
         {
             curtainAllOff(NULL, ui_curtainOnImg, ui_curtainOffImg);
         }
-        memset(g_mqtt_data.curtain_action, 0, sizeof(g_mqtt_data.curtain_action));
+        memset(g_mqtt_data.curtain1_action, 0, sizeof(g_mqtt_data.curtain1_action));
     }
 
     // 处理空调控制
-    if (strlen(g_mqtt_data.air_action) > 0)
+    if (strlen(g_mqtt_data.air1_action) > 0)
     {
-        if (strcmp(g_mqtt_data.air_action, "on") == 0)
+        if (strcmp(g_mqtt_data.air1_action, "on") == 0)
         {
             airOnOffControl(NULL, ui_airTemperature, ui_temperatureText, 1); // 1表示开启
         }
-        else if (strcmp(g_mqtt_data.air_action, "off") == 0)
+        else if (strcmp(g_mqtt_data.air1_action, "off") == 0)
         {
             airOnOffControl(NULL, ui_airTemperature, ui_temperatureText, 0); // 0表示关闭
         }
-        else if (strcmp(g_mqtt_data.air_action, "increase") == 0)
+        else if (strcmp(g_mqtt_data.air1_action, "increase") == 0)
         {
             increaseTemp(NULL, ui_airTemperature, ui_temperatureText);
         }
-        else if (strcmp(g_mqtt_data.air_action, "decrease") == 0)
+        else if (strcmp(g_mqtt_data.air1_action, "decrease") == 0)
         {
             decreaseTemp(NULL, ui_airTemperature, ui_temperatureText);
         }
-        memset(g_mqtt_data.air_action, 0, sizeof(g_mqtt_data.air_action));
+        memset(g_mqtt_data.air1_action, 0, sizeof(g_mqtt_data.air1_action));
+    }
+
+    // 主卧的组建
+    // 处理音乐控制
+    if (strlen(g_mqtt_data.music2_action) > 0)
+    {
+        if (strcmp(g_mqtt_data.music2_action, "next") == 0)
+        {
+            nextSong(NULL, ui_next1, ui_musicInfo1, ui_songName1, ui_singer1);
+        }
+        else if (strcmp(g_mqtt_data.music2_action, "prev") == 0)
+        {
+            prevSong(NULL, ui_last1, ui_musicInfo1, ui_songName1, ui_singer1);
+        }
+        else if (strcmp(g_mqtt_data.music2_action, "on") == 0)
+        {
+            playSong(NULL, ui_play1, ui_musicInfo1, ui_songName1, ui_singer1);
+        }
+        else if (strcmp(g_mqtt_data.music2_action, "off") == 0)
+        {
+            playSong(NULL, ui_play1, ui_musicInfo1, ui_songName1, ui_singer1);
+        }
+        memset(g_mqtt_data.music2_action, 0, sizeof(g_mqtt_data.music2_action));
+    }
+
+    // 处理灯光控制
+    if (strlen(g_mqtt_data.light2_action) > 0)
+    {
+        if (strcmp(g_mqtt_data.light2_action, "on") == 0)
+        {
+            lightAllOn(NULL, ui_lightOnImg1, ui_lightOffImg1, ui_lightSliderValue1, ui_lightSlider1);
+        }
+        else if (strcmp(g_mqtt_data.light2_action, "off") == 0)
+        {
+            lightAllOff(NULL, ui_lightOnImg1, ui_lightOffImg1, ui_lightSliderValue1, ui_lightSlider1);
+        }
+        memset(g_mqtt_data.light2_action, 0, sizeof(g_mqtt_data.light2_action));
+    }
+
+    // 处理窗帘控制
+    if (strlen(g_mqtt_data.curtain2_action) > 0)
+    {
+        if (strcmp(g_mqtt_data.curtain2_action, "on") == 0 || strcmp(g_mqtt_data.curtain2_action, "open") == 0)
+        {
+            curtainAllOn(NULL, ui_curtainOnImg1, ui_curtainOffImg1);
+        }
+        else if (strcmp(g_mqtt_data.curtain2_action, "off") == 0 || strcmp(g_mqtt_data.curtain2_action, "close") == 0)
+        {
+            curtainAllOff(NULL, ui_curtainOnImg1, ui_curtainOffImg1);
+        }
+        memset(g_mqtt_data.curtain2_action, 0, sizeof(g_mqtt_data.curtain2_action));
+    }
+
+    // 处理空调控制
+    if (strlen(g_mqtt_data.air2_action) > 0)
+    {
+        if (strcmp(g_mqtt_data.air2_action, "on") == 0)
+        {
+            airOnOffControl(NULL, ui_airTemperature1, ui_temperatureText1, 1); // 1表示开启
+        }
+        else if (strcmp(g_mqtt_data.air2_action, "off") == 0)
+        {
+            airOnOffControl(NULL, ui_airTemperature1, ui_temperatureText1, 0); // 0表示关闭
+        }
+        else if (strcmp(g_mqtt_data.air2_action, "increase") == 0)
+        {
+            increaseTemp(NULL, ui_airTemperature1, ui_temperatureText1);
+        }
+        else if (strcmp(g_mqtt_data.air2_action, "decrease") == 0)
+        {
+            decreaseTemp(NULL, ui_airTemperature1, ui_temperatureText1);
+        }
+        memset(g_mqtt_data.air2_action, 0, sizeof(g_mqtt_data.air2_action));
+    }
+
+    // 次卧的组建
+    // 处理音乐控制
+    if (strlen(g_mqtt_data.music3_action) > 0)
+    {
+        if (strcmp(g_mqtt_data.music3_action, "next") == 0)
+        {
+            nextSong(NULL, ui_next2, ui_musicInfo2, ui_songName2, ui_singer2);
+        }
+        else if (strcmp(g_mqtt_data.music3_action, "prev") == 0)
+        {
+            prevSong(NULL, ui_last2, ui_musicInfo2, ui_songName2, ui_singer2);
+        }
+        else if (strcmp(g_mqtt_data.music3_action, "on") == 0)
+        {
+            playSong(NULL, ui_play2, ui_musicInfo2, ui_songName2, ui_singer2);
+        }
+        else if (strcmp(g_mqtt_data.music3_action, "off") == 0)
+        {
+            playSong(NULL, ui_play2, ui_musicInfo2, ui_songName2, ui_singer2);
+        }
+        memset(g_mqtt_data.music3_action, 0, sizeof(g_mqtt_data.music3_action));
+    }
+
+    // 处理灯光控制
+    if (strlen(g_mqtt_data.light3_action) > 0)
+    {
+        if (strcmp(g_mqtt_data.light3_action, "on") == 0)
+        {
+            lightAllOn(NULL, ui_lightOnImg2, ui_lightOffImg2, ui_lightSliderValue2, ui_lightSlider2);
+        }
+        else if (strcmp(g_mqtt_data.light3_action, "off") == 0)
+        {
+            lightAllOff(NULL, ui_lightOnImg2, ui_lightOffImg2, ui_lightSliderValue2, ui_lightSlider2);
+        }
+        memset(g_mqtt_data.light3_action, 0, sizeof(g_mqtt_data.light3_action));
+    }
+
+    // 处理窗帘控制
+    if (strlen(g_mqtt_data.curtain3_action) > 0)
+    {
+        if (strcmp(g_mqtt_data.curtain3_action, "on") == 0 || strcmp(g_mqtt_data.curtain3_action, "open") == 0)
+        {
+            curtainAllOn(NULL, ui_curtainOnImg2, ui_curtainOffImg2);
+        }
+        else if (strcmp(g_mqtt_data.curtain3_action, "off") == 0 || strcmp(g_mqtt_data.curtain3_action, "close") == 0)
+        {
+            curtainAllOff(NULL, ui_curtainOnImg2, ui_curtainOffImg2);
+        }
+        memset(g_mqtt_data.curtain3_action, 0, sizeof(g_mqtt_data.curtain3_action));
+    }
+
+    // 处理空调控制
+    if (strlen(g_mqtt_data.air3_action) > 0)
+    {
+        if (strcmp(g_mqtt_data.air3_action, "on") == 0)
+        {
+            airOnOffControl(NULL, ui_airTemperature2, ui_temperatureText2, 1); // 1表示开启
+        }
+        else if (strcmp(g_mqtt_data.air3_action, "off") == 0)
+        {
+            airOnOffControl(NULL, ui_airTemperature2, ui_temperatureText2, 0); // 0表示关闭
+        }
+        else if (strcmp(g_mqtt_data.air3_action, "increase") == 0)
+        {
+            increaseTemp(NULL, ui_airTemperature2, ui_temperatureText2);
+        }
+        else if (strcmp(g_mqtt_data.air3_action, "decrease") == 0)
+        {
+            decreaseTemp(NULL, ui_airTemperature2, ui_temperatureText2);
+        }
+        memset(g_mqtt_data.air3_action, 0, sizeof(g_mqtt_data.air3_action));
     }
 
     g_mqtt_data.need_update = 0;
@@ -142,31 +301,32 @@ void on_message(struct mosquitto *obj, void *arg, const struct mosquitto_message
         if (light != NULL)
         {
             printf("设置灯光动作: %s\n", light);
-            strncpy(g_mqtt_data.light_action, light, sizeof(g_mqtt_data.light_action) - 1);
+            strncpy(g_mqtt_data.light1_action, light, sizeof(g_mqtt_data.light1_action) - 1);
             g_mqtt_data.need_update = 1;
         }
 
         if (curtain != NULL)
         {
             printf("设置窗帘动作: %s\n", curtain);
-            strncpy(g_mqtt_data.curtain_action, curtain, sizeof(g_mqtt_data.curtain_action) - 1);
+            strncpy(g_mqtt_data.curtain1_action, curtain, sizeof(g_mqtt_data.curtain1_action) - 1);
             g_mqtt_data.need_update = 1;
         }
 
         if (music != NULL)
         {
             printf("设置音乐动作: %s\n", music);
-            strncpy(g_mqtt_data.music_action, music, sizeof(g_mqtt_data.music_action) - 1);
+            strncpy(g_mqtt_data.music1_action, music, sizeof(g_mqtt_data.music1_action) - 1);
             g_mqtt_data.need_update = 1;
         }
 
         if (air != NULL)
         {
             printf("设置空调动作: %s\n", air);
-            strncpy(g_mqtt_data.air_action, air, sizeof(g_mqtt_data.air_action) - 1);
+            strncpy(g_mqtt_data.air1_action, air, sizeof(g_mqtt_data.air1_action) - 1);
             g_mqtt_data.need_update = 1;
         }
     }
+
     if (Room1_obj != NULL)
     {
         printf("找到主卧\n");
@@ -185,28 +345,81 @@ void on_message(struct mosquitto *obj, void *arg, const struct mosquitto_message
         if (light != NULL)
         {
             printf("设置灯光动作: %s\n", light);
-            strncpy(g_mqtt_data.light_action, light, sizeof(g_mqtt_data.light_action) - 1);
+            strncpy(g_mqtt_data.light2_action, light, sizeof(g_mqtt_data.light2_action) - 1);
             g_mqtt_data.need_update = 1;
         }
 
         if (curtain != NULL)
         {
             printf("设置窗帘动作: %s\n", curtain);
-            strncpy(g_mqtt_data.curtain_action, curtain, sizeof(g_mqtt_data.curtain_action) - 1);
+            strncpy(g_mqtt_data.curtain2_action, curtain, sizeof(g_mqtt_data.curtain2_action) - 1);
             g_mqtt_data.need_update = 1;
         }
 
         if (music != NULL)
         {
             printf("设置音乐动作: %s\n", music);
-            strncpy(g_mqtt_data.music_action, music, sizeof(g_mqtt_data.music_action) - 1);
+            strncpy(g_mqtt_data.music2_action, music, sizeof(g_mqtt_data.music2_action) - 1);
             g_mqtt_data.need_update = 1;
         }
 
         if (air != NULL)
         {
             printf("设置空调动作: %s\n", air);
-            strncpy(g_mqtt_data.air_action, air, sizeof(g_mqtt_data.air_action) - 1);
+            // 验证空调动作是否有效
+            if (strcmp(air, "on") == 0 || strcmp(air, "off") == 0 ||
+                strcmp(air, "increase") == 0 || strcmp(air, "decrease") == 0)
+            {
+                strncpy(g_mqtt_data.air2_action, air, sizeof(g_mqtt_data.air2_action) - 1);
+                g_mqtt_data.need_update = 1;
+            }
+            else
+            {
+                printf("警告：主卧空调收到无效动作 '%s'，忽略处理\n", air);
+            }
+        }
+    }
+
+    if (Room2_obj != NULL)
+    {
+        printf("找到次卧\n");
+
+        cJSON *Room2_light = cJSON_GetObjectItem(Room2_obj, "light");
+        cJSON *Room2_curtain = cJSON_GetObjectItem(Room2_obj, "curtain");
+        cJSON *Room2_music = cJSON_GetObjectItem(Room2_obj, "music");
+        cJSON *Room2_air = cJSON_GetObjectItem(Room2_obj, "air");
+
+        char *light = cJSON_GetStringValue(Room2_light);
+        char *curtain = cJSON_GetStringValue(Room2_curtain);
+        char *music = cJSON_GetStringValue(Room2_music);
+        char *air = cJSON_GetStringValue(Room2_air);
+
+        // 线程安全：只设置标志，不直接操作UI
+        if (light != NULL)
+        {
+            printf("设置灯光动作: %s\n", light);
+            strncpy(g_mqtt_data.light3_action, light, sizeof(g_mqtt_data.light3_action) - 1);
+            g_mqtt_data.need_update = 1;
+        }
+
+        if (curtain != NULL)
+        {
+            printf("设置窗帘动作: %s\n", curtain);
+            strncpy(g_mqtt_data.curtain3_action, curtain, sizeof(g_mqtt_data.curtain3_action) - 1);
+            g_mqtt_data.need_update = 1;
+        }
+
+        if (music != NULL)
+        {
+            printf("设置音乐动作: %s\n", music);
+            strncpy(g_mqtt_data.music3_action, music, sizeof(g_mqtt_data.music3_action) - 1);
+            g_mqtt_data.need_update = 1;
+        }
+
+        if (air != NULL)
+        {
+            printf("设置空调动作: %s\n", air);
+            strncpy(g_mqtt_data.air3_action, air, sizeof(g_mqtt_data.air3_action) - 1);
             g_mqtt_data.need_update = 1;
         }
     }
